d99c09be45ddc2f8c35a9a5d0da3006a5dc6ce99
Geert Michiels <>
2020-01-04 18:35:01 +0100

    added clang-format file + refactored main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -31,11 +31,50 @@ HixMQTT g_mqtt(Secret::WIFI_SSID,
                g_config.getDeviceTag());
 
 
+//////////////////////////////////////////////////////////////////////////////////
+// Helper functions
+//////////////////////////////////////////////////////////////////////////////////
+
 void resetWithMessage(const char * szMessage) {
     Serial.println(szMessage);
     delay(2000);
     ESP.reset();
 }
+
+void configureOTA() {
+    Serial.println("Configuring OTA, my hostname:");
+    Serial.println(g_mqtt.getMqttClientName());
+    ArduinoOTA.setHostname(g_mqtt.getMqttClientName());
+    ArduinoOTA.setPort(8266);
+    //setup handlers
+    ArduinoOTA.onStart([]() {
+        Serial.println("OTA -> Start");
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("OTA -> End");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("OTA -> Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("OTA -> Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+            Serial.println("OTA -> Auth Failed");
+        else if (error == OTA_BEGIN_ERROR)
+            Serial.println("OTA -> Begin Failed");
+        else if (error == OTA_CONNECT_ERROR)
+            Serial.println("OTA -> Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR)
+            Serial.println("OTA -> Receive Failed");
+        else if (error == OTA_END_ERROR)
+            Serial.println("OTA -> End Failed");
+    });
+    ArduinoOTA.begin();
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+// Setup
+//////////////////////////////////////////////////////////////////////////////////
 
 void setup() {
     Serial.begin(115200);
@@ -70,36 +109,9 @@ void setup() {
     Serial.println(F("Setup complete"));
 }
 
-void configureOTA() {
-    Serial.println("Configuring OTA, my hostname:");
-    Serial.println(g_mqtt.getMqttClientName());
-    ArduinoOTA.setHostname(g_mqtt.getMqttClientName());
-    ArduinoOTA.setPort(8266);
-    //setup handlers
-    ArduinoOTA.onStart([]() {
-        Serial.println("OTA -> Start");
-    });
-    ArduinoOTA.onEnd([]() {
-        Serial.println("OTA -> End");
-    });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("OTA -> Progress: %u%%\r", (progress / (total / 100)));
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
-        Serial.printf("OTA -> Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR)
-            Serial.println("OTA -> Auth Failed");
-        else if (error == OTA_BEGIN_ERROR)
-            Serial.println("OTA -> Begin Failed");
-        else if (error == OTA_CONNECT_ERROR)
-            Serial.println("OTA -> Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR)
-            Serial.println("OTA -> Receive Failed");
-        else if (error == OTA_END_ERROR)
-            Serial.println("OTA -> End Failed");
-    });
-    ArduinoOTA.begin();
-}
+//////////////////////////////////////////////////////////////////////////////////
+// Loop
+//////////////////////////////////////////////////////////////////////////////////
 
 void loop() {
     //other loop functions
@@ -128,6 +140,10 @@ void loop() {
         g_mqtt.publishStatusValues(g_nCurrentCO2, g_fCurrentTemp);
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////////
+// Required by the MQTT library
+//////////////////////////////////////////////////////////////////////////////////
 
 void onConnectionEstablished() {
     //setup OTA
