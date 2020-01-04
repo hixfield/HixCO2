4a408fe20ac48cf48df46f58ea958a4636ca2f53
Geert Michiels <>
2020-01-04 12:16:47 +0100

    initial commit

---
+++ src/secret.cpp
@@ -0,0 +1,6 @@
+#include "secret.h"
+
+const char * Secret::WIFI_SSID   = "xxxxxxxxxx";
+const char * Secret::WIFI_PWD    = "xxxxxxxxxx";
+const char * Secret::MQTT_SERVER = "xxxxxxxxxx";
+const char * Secret::CONFIGPWD   = "bornem1973";
